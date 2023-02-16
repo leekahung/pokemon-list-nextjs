0f0599fb43440bfa32aa8887f526fa9708f16890
Ka Hung Lee <lee.kahung414@gmail.com>
2023-02-16 01:18:50 -0800

    Preparing for deployment on GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? "/pokemon-list-nextjs/" : "/",
+  basePath: !debug ? "/pokemon-list-nextjs" : "",
 };
 
 module.exports = nextConfig;
